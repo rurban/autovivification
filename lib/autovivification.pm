0815af96defabe600917fb0ba73a676235884a36
Vincent Pit <vince@profvince.com>
2014-10-04 19:07:14 +0200

    This is 0.13

--- lib/autovivification.pm
+++ lib/autovivification.pm
@@ -11,13 +11,13 @@ autovivification - Lexically disable autovivification.
 
 =head1 VERSION
 
-Version 0.12
+Version 0.13
 
 =cut
 
 our $VERSION;
 BEGIN {
- $VERSION = '0.12';
+ $VERSION = '0.13';
 }
 
 =head1 SYNOPSIS
