9686b1070ec31c97d43957bad69f4bf0e70590b1
Vincent Pit <vince@profvince.com>
2015-05-14 17:07:44 +0200

    Bump copyright year

--- lib/autovivification.pm
+++ lib/autovivification.pm
@@ -246,7 +246,7 @@ Matt S. Trout asked for it.
 
 =head1 COPYRIGHT & LICENSE
 
-Copyright 2009,2010,2011,2012,2013,2014 Vincent Pit, all rights reserved.
+Copyright 2009,2010,2011,2012,2013,2014,2015 Vincent Pit, all rights reserved.
 
 This program is free software; you can redistribute it and/or modify it under the same terms as Perl itself.
 
